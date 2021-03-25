14d0864925ad4e6faff6a719ff8ebb29a7ca8645
matankleiner <matankleiner@gmail.com>
2021-03-25 23:11:44 +0200

    update

--- feature_extraction/knn_classification.py
+++ feature_extraction/knn_classification.py
@@ -14,16 +14,19 @@ import time
 
 start = time.time()
 
-RESULT_PATH = "predicted_class_embedded_test.csv" 
-NEIGHBORS = 5 
+NEIGHBORS = 7
+RESULT_PATH = "predicted_class_embedded_test_K=7.csv"
 
 test = torch.load('embedded_test.pt', map_location=lambda storage, loc: storage) 
 data = torch.load('embedded_data.pt', map_location=lambda storage, loc: storage)
 labels = torch.load('labels.pt', map_location=lambda storage, loc: storage) 
 
 neigh = KNeighborsClassifier(n_neighbors = NEIGHBORS) 
-neigh.fit(data, labels) 
+neigh.fit(data, labels)
 
+"""
+# if you would like to predict the nearest neighbor for the whole data set unmark this section 
+# and mark the next section
 predict_class_list = []
 print("Predicting the class for each test set image using K-NN:")
 for i in tqdm(range(len(test))): 
@@ -32,6 +35,21 @@ for i in tqdm(range(len(test))):
 
 predict_class_df = pd.DataFrame(predict_class_list)
 predict_class_df.to_csv(RESULT_PATH) # save results as CSV file 
+"""
+
+# predict the nearest neighbors ONLY for the landmarks images
+test_df = pd.read_csv("/data/yuvalsnir@ef.technion.ac.il/test_set_kaggle_2019/test.csv")
+landmark_indices = test_df["landmarks"] != 0
+landmark_indices = landmark_indices[landmark_indices].index
+
+predict_class_list = []
+print("Predicting the class for each test set image using K-NN:")
+for i in tqdm(landmark_indices):
+    predict_class = neigh.predict([test[i]])
+    predict_class_list.append(predict_class)
+
+predict_class_df = pd.DataFrame(predict_class_list)
+predict_class_df.to_csv(RESULT_PATH) # save results as CSV file
 
 end = time.time()
 print("time: {}".format(end-start))
--- feature_extraction/nearest_neighbors.py
+++ feature_extraction/nearest_neighbors.py
@@ -1,8 +1,7 @@
 # !/usr/bin/env python3.6
 
 """
-In this script we're using NearestNeighbors (from scklearn) to calculate the nearest neighbors and the distances 
-of each test set image. 
+In this script we're using KNN Classifier (from scklearn) to predict the correct class for each test set image. 
 Because of the very large embedded data set (4.4 Gb of data) and the very large test set (0.33 Gb of data) the prediction
 process was very long and took 121272.02406287193 seconds, i.e. 1.4 days. 
 """
@@ -16,9 +15,9 @@ import time
 
 start = time.time()
 
-NEIGHBORS = 5 
-DIST_PATH = "dist_embedded_test.csv" 
-NN_PATH = "nearest_neighbor_embedded_test.csv" 
+NEIGHBORS = 7
+DIST_PATH = "dist_embedded_test_K=7.csv"
+NN_PATH = "nearest_neighbor_embedded_test_K=7.csv"
 
 test = torch.load('embedded_test.pt', map_location=lambda storage, loc: storage) 
 data = torch.load('embedded_data.pt', map_location=lambda storage, loc: storage)
@@ -26,6 +25,31 @@ data = torch.load('embedded_data.pt', map_location=lambda storage, loc: storage)
 neigh = NearestNeighbors(n_neighbors=NEIGHBORS)
 neigh.fit(data)
 
+# check the nearest neighbors ONLY for the landmarks images
+test_df = pd.read_csv("/data/yuvalsnir@ef.technion.ac.il/test_set_kaggle_2019/test.csv")
+landmark_indices = test_df["landmarks"] != 0
+landmark_indices = landmark_indices[landmark_indices].index
+
+dist_list = []
+nearest_neighbor_list = []
+print("Calculating each test set image nearest neighbors and distances to each neighbor:")
+for i in tqdm(landmark_indices):
+    dist, nearest_neighbor = neigh.kneighbors([test[i]])
+    dist_list.append(dist)
+    nearest_neighbor_list.append(nearest_neighbor)
+
+dist_np = np.array(dist_list)
+dist_np.shape = -1, NEIGHBORS # reshape the list
+dist_df = pd.DataFrame(dist_np)
+dist_df.to_csv(DIST_PATH) # save dist as CSV file
+nearest_neighbor_np = np.array(nearest_neighbor_list)
+nearest_neighbor_np.shape = -1, NEIGHBORS # reshape the list
+nearest_neighbor_df = pd.DataFrame(nearest_neighbor_np)
+nearest_neighbor_df.to_csv(NN_PATH) # save nearest_neighbors as CSV file
+
+"""
+# if you would like to check the nearest neighbor for the whole data set unmark this section 
+# and mark the next section
 dist_list = []
 nearest_neighbor_list = []
 print("Calculating each test set image nearest neighbors and distances to each neighbor:")
@@ -35,13 +59,14 @@ for i in tqdm(range(len(test))):
     nearest_neighbor_list.append(nearest_neighbor)
 
 dist_np = np.array(dist_list)
-dist_np.shape = -1, NEIGHBORS # reshape the list
+dist_np.shape = -1, NEIGHBORS # reshape the list 
 dist_df = pd.DataFrame(dist_np)
 dist_df.to_csv(DIST_PATH) # save dist as CSV file 
 nearest_neighbor_np = np.array(nearest_neighbor_list)
 nearest_neighbor_np.shape = -1, NEIGHBORS # reshape the list
 nearest_neighbor_df = pd.DataFrame(nearest_neighbor_np)
 nearest_neighbor_df.to_csv(NN_PATH) # save nearest_neighbors as CSV file 
+"""
 
 end = time.time()
 print("time: {}".format(end-start))
